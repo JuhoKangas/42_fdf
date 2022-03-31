490c29e2a86246ec7e1e72d15ae9373115ec98b6
Juho Kangas <jkangas@student.hive.fi>
2022-03-31 13:22:41 +0300

    add dda line drawing algorithm, add fdf.h

---
+++ includes/fdf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkangas <jkangas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 11:13:27 by jkangas           #+#    #+#             */
+/*   Updated: 2022/03/31 13:12:12 by jkangas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+#include <math.h>
+#include <stdio.h>
+#include "../libft/libft.h"
+
+typedef struct s_coord
+{
+	int		x1;
+	int		x2;
+	int		y1;
+	int		y2;
+}			t_coord;
+
+int	rgb_to_int(double r, double g, double b);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: jkangas <jkangas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 11:23:07 by jkangas           #+#    #+#             */
-/*   Updated: 2022/03/29 14:46:56 by jkangas          ###   ########.fr       */
+/*   Updated: 2022/03/31 13:14:07 by jkangas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"	
+#include "../includes/fdf.h"
 
 int rgb_to_int(double r, double g, double b)
 {
@@ -21,15 +22,53 @@ int rgb_to_int(double r, double g, double b)
     return (color);
 }
 
+void	ft_dda(t_coord coord, void *mlx, void *win)
+{
+	int		dx;
+	int		dy;
+	int 	steps;
+	float	x_incr;
+	float	y_incr;
+	float	x;
+	float	y;
+	int		i;
+
+	dx = coord.x2 - coord.x1;
+	dy = coord.y2 - coord.y1;
+
+	if (ft_abs(dx) > ft_abs(dy))
+		steps = dx;
+	else
+		steps = dy;
+	
+	x_incr = dx / (float) steps;
+	y_incr = dy / (float) steps;
+
+	x = coord.x1;
+	y = coord.y1;
+
+	i = 0;
+	while (i <= steps)
+	{
+		mlx_pixel_put(mlx, win, x, y, 0xffffff);
+		x += x_incr;
+		y += y_incr;
+		i++;
+	}
+}
+
 int	main()
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	int		x;
-	int		y;
-	int		color;
 	int		win_x;
 	int		win_y;
+	t_coord coord;
+
+	coord.x1 = 250;
+	coord.y1 = 500;
+	coord.x2 = 600;
+	coord.y2 = 600;
 
 	win_x = 1000;
 	win_y = 1000;
@@ -37,18 +76,7 @@ int	main()
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, win_x, win_y, "window");
 
-	x = 0;
-	color = 0;
-	while ( x < win_x)
-	{
-		y = 0;
-		while (y < win_y)
-		{
-			mlx_pixel_put(mlx_ptr, win_ptr, y, x, color);
-			y++;
-		}
-		x++;
-		color += 100;
-	}
+	ft_dda(coord, mlx_ptr, win_ptr);
+
 	mlx_loop(mlx_ptr);
 }
