7bcd5c09401762fc27609be6cca40aaa94dcd98e
Juho Kangas <jkangas@student.hive.fi>
2022-05-02 17:12:44 +0300

    add one more string prompt, change the location of the text

--- src/ui_controller.c
+++ src/ui_controller.c
@@ -6,7 +6,7 @@
 /*   By: jkangas <jkangas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 14:14:51 by jkangas           #+#    #+#             */
-/*   Updated: 2022/05/02 14:31:50 by jkangas          ###   ########.fr       */
+/*   Updated: 2022/05/02 17:12:02 by jkangas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ void	display_ui(t_fdf *data)
 	char	*str1;
 	char	*str2;
 	char	*str3;
+	char	*str4;
 
 	str1 = "Press +/- to zoom";
 	str2 = "Use arrow keys to move fdf around";
-	str3 = "Press esc to exit";
-	mlx_string_put(data->mlx, data->win, 20, 100, rgb_to_int(1, 1, 1), str1);
-	mlx_string_put(data->mlx, data->win, 20, 115, rgb_to_int(1, 1, 1), str2);
-	mlx_string_put(data->mlx, data->win, 20, 130, rgb_to_int(1, 1, 1), str3);
+	str3 = "Use space to change the projection";
+	str4 = "Press esc to exit";
+	mlx_string_put(data->mlx, data->win, 20, 50, rgb_to_int(1, 1, 1), str1);
+	mlx_string_put(data->mlx, data->win, 20, 65, rgb_to_int(1, 1, 1), str2);
+	mlx_string_put(data->mlx, data->win, 20, 80, rgb_to_int(1, 1, 1), str3);
+	mlx_string_put(data->mlx, data->win, 20, 95, rgb_to_int(1, 1, 1), str4);
 }
